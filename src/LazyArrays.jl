776cf6658736b16b6ec51cdd2e4b13cf6f45f8ce
Sheehan Olver <solver@mac.com>
2019-11-18 11:41:11 +0000

    Import lazy_getindex

--- src/LazyArrays.jl
+++ src/LazyArrays.jl
@@ -49,7 +49,7 @@ import ArrayLayouts: MatMulVecAdd, MatMulMatAdd, MulAdd, Lmul, Rmul, Ldiv,
                         transposelayout, conjlayout, sublayout, triangularlayout, triangulardata,
                         reshapedlayout, diagonallayout, adjointlayout, sub_materialize,
                         check_mul_axes, _mul_eltype, check_ldiv_axes, ldivaxes, colsupport, rowsupport,
-                        _fill_lmul!, @lazylmul, scalarone, scalarzero, fillzeros, zero!
+                        _fill_lmul!, @lazylmul, scalarone, scalarzero, fillzeros, zero!, lazy_getindex
 
 if VERSION < v"1.2-"
     import Base: has_offset_axes
