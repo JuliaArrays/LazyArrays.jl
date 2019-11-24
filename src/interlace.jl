5736435ea904906652fe47a4e7b50456e2d295a7
Sheehan Olver <solver@mac.com>
2019-11-24 22:44:54 +0000

    Interlace improvements, add BroadcastArray{T,N} cases

--- src/interlace.jl
+++ src/interlace.jl
@@ -3,9 +3,10 @@ struct Interlace{T, N, AA, INDS} <: AbstractArray{T,N}
     inds::INDS
 end
 
-Interlace(a::AbstractArray{<:AbstractArray{T,N},N}, inds) where {T,N} = Interlace{T,N,typeof(a), typeof(inds)}(a, inds)
+Interlace(a::NTuple{M,AbstractVector{T}}, inds::NTuple{M,AbstractVector{Int}}) where {T,M} = 
+    Interlace{T,1,typeof(a), typeof(inds)}(a, inds)
 
-length(A::Interlace) = sum(length(A.arrays))
+length(A::Interlace) = sum(map(length,A.arrays))
 size(A::Interlace, m) = sum(size.(A.arrays,m))
 size(A::Interlace{<:Any,1}) = (size(A,1),)
 function getindex(A::Interlace{<:Any,1}, k::Integer)
@@ -15,3 +16,8 @@ function getindex(A::Interlace{<:Any,1}, k::Integer)
     end
     throw(BoundsError(A, k))
 end
+
+Interlace(a::AbstractVector, b::AbstractVector) = 
+    Interlace((a,b), (1:2:(2length(a)-1), 2:2:2length(b)))
+
+interlace(a...) = Array(Interlace(a...))    
