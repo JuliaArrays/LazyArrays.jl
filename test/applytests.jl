c02b99af2d78e857c8e24384868e8cb869d872d0
Thibaut Lienart <tlienart@me.com>
2019-11-29 15:01:04 +0000

    Update applytests.jl

--- test/applytests.jl
+++ test/applytests.jl
@@ -40,3 +40,12 @@ end
     @test M isa ApplyArray
     @test M == exp(A)
 end
+
+@testset "copy (#85)" begin
+    Base.size(A::Applied) = (length(A.args[1]),)
+    Base.eltype(A::Applied)  = eltype(A.args[1])
+    v = ApplyVector(vec, ones(Int, 2, 2))
+    vc = copy(float.(v))
+    ve = convert(Vector, vc)
+    @test eltype(ve) == Float64
+end
