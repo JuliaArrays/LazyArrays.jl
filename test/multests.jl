8b5effe31fce714ad5469df38d5d39dd5200ed27
Sheehan Olver <solver@mac.com>
2020-09-08 22:49:55 +0100

    Update multests.jl

--- test/multests.jl
+++ test/multests.jl
@@ -1,4 +1,4 @@
-using Test, LinearAlgebra, LazyArrays, StaticArrays, FillArrays
+using Test, LinearAlgebra, LazyArrays, StaticArrays, FillArrays, Base64
 import LazyArrays: MulAdd, MemoryLayout, DenseColumnMajor, DiagonalLayout, SymTridiagonalLayout, Add, AddArray, 
                     MulStyle, MulAddStyle, Applied, ApplyStyle, Lmul, ApplyArrayBroadcastStyle, DefaultArrayApplyStyle,
                     Rmul, ApplyLayout, arguments, colsupport, rowsupport, lazymaterialize
