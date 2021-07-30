ff726c987da634456dec23ed5ce8999e74697496
Sheehan Olver <solver@mac.com>
2021-07-30 11:08:23 +0100

    Update lazymultests.jl

--- test/lazymultests.jl
+++ test/lazymultests.jl
@@ -272,4 +272,12 @@ LinearAlgebra.factorize(A::MyLazyArray) = factorize(A.data)
         M = ApplyArray(*, A, B)
         @test layout_getindex(M,[CartesianIndex(1,2),CartesianIndex(3,3)]) == M[[CartesianIndex(1,2),CartesianIndex(3,3)]] == [M[1,2], M[3,3]]
     end
+
+    @testset "(A\B)*C" begin
+        A = randn(5,5)
+        B = randn(5,5)
+        C = randn(5,5)
+        L = ApplyArray(\,A,B)
+        @test all(L*C .=== A \ (B*C))
+    end
 end
