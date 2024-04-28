a0e076f073c2368dcad7da4e9e0f7601a5cb85fb
Sheehan Olver <solver@mac.com>
2024-04-28 14:04:24 +0100

    Update LazyArraysBlockArraysExt.jl

--- ext/LazyArraysBlockArraysExt.jl
+++ ext/LazyArraysBlockArraysExt.jl
@@ -152,4 +152,56 @@ broadcasted(::LazyArrayStyle{0}, ::Type{Int}, block::Block{1}) = Int(block)
 
 Base.in(K::Block, B::BroadcastVector{<:Block,Type{Block}}) = Int(K) in B.args[1]
 
+
+###
+# Concat
+###
+
+sub_materialize(lay::ApplyLayout{typeof(vcat)}, V::AbstractVector, ::Tuple{<:BlockedUnitRange}) = blockvcat(sub_materialize.(arguments(lay, V))...)
+sub_materialize(lay::ApplyLayout{typeof(vcat)}, V::AbstractMatrix, ::Tuple{<:BlockedUnitRange,<:BlockedUnitRange}) = blockvcat(sub_materialize.(arguments(lay, V))...)
+sub_materialize(lay::ApplyLayout{typeof(vcat)}, V::AbstractMatrix, ::Tuple{<:BlockedUnitRange,<:AbstractUnitRange}) = blockvcat(sub_materialize.(arguments(lay, V))...)
+
+LazyArrays._vcat_sub_arguments(lay::ApplyLayout{typeof(vcat)}, A, V, kr::BlockSlice{<:BlockRange{1}}) =
+    arguments(lay, A)[Int.(kr.block)]
+
+
+_split2blocks(KR) = ()
+function _split2blocks(KR, ax::OneTo, C...)
+    if isempty(KR)
+        (Base.OneTo(0), _split2blocks(Block.(1:0), C...)...)
+    elseif first(KR) ≠ Block(1)
+        (Base.OneTo(0), _split2blocks((KR[1] - Block(1)):(KR[end] - Block(1)), C...)...)
+    elseif length(KR) == 1
+        (ax, _split2blocks(Block.(1:0), C...)...)
+    else
+        (ax, _split2blocks((KR[2]- Block(1)):(KR[end]-Block(1)), C...)...)
+    end
+end
+function _split2blocks(KR, A, C...)
+    M = blocklength(A)
+    if Int(last(KR)) ≤ M
+        (KR, _split2blocks(Block.(1:0), C...)...)
+    else
+        (KR[1]:Block(M), _split2blocks(Block(1):(last(KR)-Block(M)), C...)...)
+    end
+end
+
+function arguments(lay::ApplyLayout{typeof(vcat)}, V::SubArray{<:Any,1,<:Any,<:Tuple{BlockSlice{<:BlockRange1}}})
+    kr, = parentindices(V)
+    P = parent(V)
+    a = arguments(lay, P)
+    KR = _split2blocks(kr.block, axes.(a,1)...)
+    filter(!isempty,getindex.(a, KR))
+end
+
+
+function arguments(lay::ApplyLayout{typeof(vcat)}, V::SubArray{<:Any,2,<:Any,<:Tuple{BlockSlice{<:BlockRange1},BlockSlice}})
+    kr, jr = parentindices(V)
+    P = parent(V)
+    a = arguments(lay, P)
+    KR = _split2blocks(kr.block, axes.(a,1)...)
+    filter(!isempty,getindex.(a, KR, Ref(jr.block)))
+end
+
+
 end
