b2b85cb7aa24133b0652686f255d941d5a32483a
Sheehan Olver <solver@mac.com>
2024-06-03 14:56:03 +0800

    Fix copyto! with banded BroadcastArray and lazy banded \ diagonal (#310)
    * Fix copyto! with banded BroadcastArray
    
    * lazy inv banded * diagonal

--- test/bandedtests.jl
+++ test/bandedtests.jl
@@ -832,6 +832,22 @@ LinearAlgebra.lmul!(β::Number, A::PseudoBandedMatrix) = (lmul!(β, A.data); A)
         @test MemoryLayout(UpperTriangular(A)) isa TriangularLayout{'U', 'N', LazyBandedLayout}
         @test MemoryLayout(LowerTriangular(A)) isa TriangularLayout{'L', 'N', LazyBandedLayout}
     end
+
+    @testset "copyto! broadcast view" begin
+        n = 10
+        A = brand(n,n,2,1)
+        B = BroadcastArray(\, 1:n, A)
+        @test B[1:3,1:4] ≈ (1:3) .\ A[1:3,1:4]
+        @test B'[1:3,1:4] ≈ B[1:4,1:3]'
+    end
+
+    @testset "Ldiv with Diagonal" begin
+        n = 10
+        a = BroadcastArray(*, 2, 2 .+ rand(n))
+        b = BroadcastArray(*, 2, randn(n-1))
+        B = Bidiagonal(a, b, :U)
+        @test ldiv(B, Diagonal(1:n)) ≈ ApplyArray(inv,B) * Diagonal(1:n) ≈ B \ Diagonal(1:n)
+    end
 end
 
 end # module
