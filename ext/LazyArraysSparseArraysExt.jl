3175e993181416e55cd499b330fbb080328f7e79
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2025-08-21 12:39:40 +0200

    Remove unnecessary local_issparse methods
    It is sufficient to define a method for `Any` in the package, and for `AbstractArray` in the
    SparseArrays extension. Other dispatches are already implemented in SparseArrays.

--- ext/LazyArraysSparseArraysExt.jl
+++ ext/LazyArraysSparseArraysExt.jl
@@ -4,10 +4,6 @@ using SparseArrays: issparse, nnz, AbstractSparseArray
 import LazyArrays: local_issparse, local_nnz
 
 local_nnz(A::AbstractSparseArray) = nnz(A)
-
 local_issparse(A::AbstractArray) = issparse(A)
-local_issparse(A::DenseArray) = issparse(A)
-local_issparse(S::AbstractSparseArray) = issparse(S)
-
 
 end
