580ca037a8edf478dc091400a23e1e9a94e5a788
Daniel VandenHeuvel <95613936+DanielVandH@users.noreply.github.com>
2025-11-18 17:54:21 +0000

    The change

--- src/cache.jl
+++ src/cache.jl
@@ -405,7 +405,7 @@ end
 _bc_resizecacheddata!(::DualLayout{ML}, a) where {ML<:AbstractCachedLayout} =  _bc_resizecacheddata!(ML(), a)
 _bc_resizecacheddata!(_, a) = a
 _bc_resizecacheddata!(a) = _bc_resizecacheddata!(MemoryLayout(a), a)
-resize_bcargs!(bc::Broadcasted{<:CachedArrayStyle{N}}) where {N} = Broadcasted(LazyArrayStyle{N}(), bc.f, map(_bc_resizecacheddata!, bc.args)) # not broadcasted(bc.f, map(_bc_resizecacheddata!, bc.args)...) to avoid issues with StackOverflows with CacheArrayStyles
+resize_bcargs!(bc::Broadcasted{<:CachedArrayStyle{N}}) where {N} = broadcasted(bc.f, map(_bc_resizecacheddata!, bc.args)...)
 
 similar(bc::Broadcasted{<:CachedArrayStyle}, ::Type{T}) where T = CachedArray(zeros(T, axes(bc)))
 
